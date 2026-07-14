31986d0f3ab4e9e9b7c6a18a3bac8667252fe317
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 09:21:44 -0300

    Create herramientas.py

---
+++ backend/models/herramientas.py
@@ -0,0 +1,15 @@
+HERRAMIENTAS = {
+    "CNMG120408": {
+        "avance": 0.25,
+        "radio": 0.8
+    },
+    "DNMG150608": {
+        "avance": 0.18,
+        "radio": 0.8
+    },
+    "MGMN300": {
+        "avance": 0.08,
+        "ancho": 3
+    }
+}
+``

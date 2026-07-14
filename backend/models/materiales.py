1824dd2c0f2c8a795b3db1ed28f3e464b4fcc80a
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 13:09:31 -0300

    Update materiales.py

--- backend/models/materiales.py
+++ backend/models/materiales.py
@@ -1,14 +1,37 @@
 MATERIALES = {
     "SAE1045": {
-        "vc": 180
+        "descripcion": "Acero medio carbono",
+        "vc": 180,
+        "vc_fresado": 150
     },
+
     "SAE1020": {
-        "vc": 200
+        "descripcion": "Acero bajo carbono",
+        "vc": 200,
+        "vc_fresado": 170
     },
+
     "AISI304": {
-        "vc": 120
+        "descripcion": "Acero inoxidable 304",
+        "vc": 120,
+        "vc_fresado": 100
     },
+
+    "AISI316": {
+        "descripcion": "Acero inoxidable 316",
+        "vc": 110,
+        "vc_fresado": 90
+    },
+
     "ALUMINIO6061": {
-        "vc": 350
+        "descripcion": "Aluminio 6061",
+        "vc": 350,
+        "vc_fresado": 300
+    },
+
+    "SAE4140": {
+        "descripcion": "Acero aleado 4140",
+        "vc": 140,
+        "vc_fresado": 120
     }
 }

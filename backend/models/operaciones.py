356a079a82b4afcdc48e5a8b16f1112f802679a8
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 09:52:30 -0300

    Add OPERACIONES_TORNO dictionary to operaciones.py

---
+++ backend/models/operaciones.py
@@ -0,0 +1,8 @@
+OPERACIONES_TORNO = {
+    "refrentado": "Refrentado",
+    "cilindrado": "Cilindrado",
+    "g71": "Desbaste G71",
+    "g76": "Roscado G76",
+    "ranurado": "Ranurado",
+    "tronzado": "Tronzado"
+}

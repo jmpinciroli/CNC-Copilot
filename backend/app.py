9cddec5a9d702cc20e1df9706b1825dd21544d15
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 11:36:51 -0300

    Refactor FastAPI app initialization
    Refactor app initialization to import from api module.

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI(
-    title="CNC Copilot API",
-    version="0.1"
-)
-
-@app.get("/")
-def home():
-    return {
-        "app": "CNC Copilot",
-        "version": "0.1"
-    }
+from api import app

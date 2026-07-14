77cb927eb3125c5f96a3fb60af1e0cd99cd2d18a
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 10:34:02 -0300

    Refactor perfil data structure into Perfil class

--- backend/models/perfil.py
+++ backend/models/perfil.py
@@ -1,7 +1,20 @@
-[
-    {"tipo":"linea","x":100,"z":0},
-    {"tipo":"linea","x":80,"z":-50},
-    {"tipo":"radio","r":5},
-    {"tipo":"linea","x":60,"z":-90}
-]
-`
+class Perfil:
+
+    def __init__(self):
+        self.segmentos = []
+
+    def agregar_linea(self, x, z):
+        self.segmentos.append({
+            "tipo": "linea",
+            "x": x,
+            "z": z
+        })
+
+    def agregar_radio(self, radio):
+        self.segmentos.append({
+            "tipo": "radio",
+            "r": radio
+        })
+
+    def obtener_segmentos(self):
+        return self.segmentos

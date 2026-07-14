e7dd59cddef22c9c7ba7b78a03b4609d8a23b018
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 09:11:17 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="CNC Copilot API",
+    version="0.1"
+)
+
+@app.get("/")
+def home():
+    return {
+        "app": "CNC Copilot",
+        "version": "0.1"
+    }

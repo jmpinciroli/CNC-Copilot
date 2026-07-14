3ae8a2c680f47f476a248ca770c0d836ab394c1e
jmpinciroli <josempinciroli@gmail.com>
2026-07-14 11:31:37 -0300

    Add generar function to generate CNC code

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,27 @@
+function generar() {
 
+    const codigo = `% 
+O1000
+
+G21
+G18
+G40
+G99
+
+T0101
+
+G97 S800 M03
+
+G00 X102 Z2
+
+G01 X100 Z0 F0.25
+
+G01 X80 Z-120
+
+M30
+
+%
+`;
+
+    document.getElementById("resultado").textContent = codigo;
+}
